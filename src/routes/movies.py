bbe0330708d85eee01c67bc4d6ed5221412a5ae8
Бронислав <atlantwindow@gmail.com>
2025-05-28 15:44:21 +0300

    mission completed

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -1,10 +1,59 @@
+from typing import Annotated
+
 from fastapi import APIRouter, Depends, HTTPException, Query
-from sqlalchemy import select
+from sqlalchemy import select, func
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import get_db, MovieModel
-
+from schemas.movies import MovieDetailResponseSchema, MovieListResponseSchema
 
 router = APIRouter()
 
-# Write your code here
+
+@router.get("/movies/", response_model=MovieListResponseSchema)
+async def get_movies(
+        page: Annotated[int, Query(ge=1)] = 1,
+        per_page: Annotated[int, Query(ge=1, le=20)] = 10,
+        db: AsyncSession = Depends(get_db),
+):
+    offset = (page - 1) * per_page
+
+    total_count = await db.scalar(select(func.count()).select_from(MovieModel))
+    if total_count == 0:
+        raise HTTPException(status_code=404, detail="No movies found.")
+
+    total_pages = (total_count + per_page - 1) // per_page
+
+    query = select(MovieModel).offset(offset).limit(per_page)
+    result = await db.execute(query)
+    movies = result.scalars().all()
+
+    base_url = "/theater/movies/"
+    prev_page = f"{base_url}?page={page - 1}&per_page={per_page}" if page > 1 else None
+    next_page = f"{base_url}?page={page + 1}&per_page={per_page}" if page < total_pages else None
+
+    return MovieListResponseSchema(
+        movies=movies,
+        prev_page=prev_page,
+        next_page=next_page,
+        total_pages=total_pages,
+        total_items=total_count,
+    )
+
+
+@router.get("/movies/{movie_id}/", response_model=MovieDetailResponseSchema)
+async def get_movie(
+        movie_id: int,
+        db: AsyncSession = Depends(get_db),
+):
+    query = select(MovieModel).where(MovieModel.id == movie_id)
+    result = await db.execute(query)
+    movie = result.scalar_one_or_none()
+
+    if movie is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Movie with the given ID was not found.",
+        )
+
+    return movie
